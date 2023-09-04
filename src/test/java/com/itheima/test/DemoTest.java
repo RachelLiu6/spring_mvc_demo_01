f1489b999b75f774cd01ec703c44772abde8583f
RachelLiu6 <ruixiliu6@gmail.com>
2023-09-03 17:16:42 -0700

    set up project

---
+++ src/test/java/com/itheima/test/DemoTest.java
@@ -0,0 +1,20 @@
+package com.itheima.test;
+
+import org.junit.Test;
+
+public class DemoTest {
+    @Test
+    public void test1(){
+        System.out.println("hello test1");
+    }
+
+    @Test
+    public void test2(){
+        System.out.println("hello test2");
+    }
+
+    @Test
+    public void test3(){
+        System.out.println("hello test3");
+    }
+}
